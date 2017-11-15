a8994a2ffed0878d35356c63ebda77d82f12c4e2
Belal kamara <belalkamara@Belals-MacBook-Air.local>
2017-11-15 10:13:56 -0600

    Refactored alerts into help method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -59,5 +59,24 @@ module ApplicationHelper
     "active" if current_page? path
   end
 
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+    
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Belal Kamara's Portfolio", sticky: false)
+  end
+
+
+
+
+
+
+
+
 end
 
