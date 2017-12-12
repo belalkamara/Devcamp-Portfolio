fe08d520074f6278a160d7f1022bca7f5e6ef4c3
Belal kamara <belalkamara@Belals-MacBook-Air.local>
2017-12-11 23:13:23 -0600

    Converted static assets to equal true

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module DevcampPortfolio
   class Application < Rails::Application
     config.eager_load_paths << "#{Rails.root}/lib"
     config.secret_key_base = ENV["SECRET_KEY_BASE"]
+    config.serve_static_assets = true
   end
 end
 
