aeea96f7fcdf8ee7b4ca6fb0237e1a6e08fc7e82
Belal kamara <belalkamara@Belals-MacBook-Air.local>
2017-12-13 20:59:04 -0600

    Removed the 2 lines I added in the production file because bug is still there

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,8 +21,8 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_assets = true
-  config.assets.digest = true
+  #config.serve_static_assets = true
+  #config.assets.digest = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
