42039d1c65b51899eaecc072235d80d4d409206d
Belal Kamara <belalkamara@gmail.com>
2018-07-17 20:43:16 -0500

    Fixing errors in Portfolio

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
@@ -93,4 +93,7 @@ Rails.application.configure do
 
   config.action_cable.allowed_request_origins = ['https://bk-devcamp-portfolio.herokuapp.com', 'http://bk-devcamp-portfolio.herokuapp.com']
   config.action_cable.url = "wss://bk-devcamp-portfolio.herokuapp.com/cable"
+
+  config.serve_static_assets = true
+  config.assets.digest = true
 end
